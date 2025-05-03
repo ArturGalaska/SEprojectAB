1e1ad049cd47b94725d7f006a698843306be0795
Artur <a.gaaska@student.maastrichtuniversity.nl>
2025-05-03 20:08:15 +0200

    triggered alert is done

--- src/main/java/com/alerts/TriggeredAlertChecker.java
+++ src/main/java/com/alerts/TriggeredAlertChecker.java
@@ -14,12 +14,15 @@ public class TriggeredAlertChecker implements AlertChecker {
     public TriggeredAlertChecker(){
 
     }
+    
     @Override
     public void check(Patient patient, List<PatientRecord> patientData, AlertGenerator generator){
-        
-        if(buttonPressed==true){
-            generator.trigger(new Alert(patient.getPatientId(), "Button Triggered",0 ));
+        for(int i =0;i<patientData.size();i++){
+            PatientRecord record = patientData.get(i);
+            if(record.getRecordType().equals("Alert")){
+                buttonPressed=true;
+                generator.trigger(new Alert(patient.getPatientId(), "Alert, button was triggered.", record.getTimestamp()));
+            }
         }
-    
     }
 }
