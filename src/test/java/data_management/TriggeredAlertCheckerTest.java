4482bdfb97c85c24c675dc7d403d3d2ae0ba56e8
Bartek <b.kaczor@student.maastrichtuniversity.nl>
2025-05-04 20:14:10 +0200

    Implemented TriggeredAlertCheckerTest

---
+++ src/test/java/data_management/TriggeredAlertCheckerTest.java
@@ -0,0 +1,64 @@
+package data_management;
+
+
+import com.alerts.Alert;
+import com.alerts.TriggeredAlertChecker;
+import com.alerts.AlertGenerator;
+
+
+import com.data_management.DataStorage;
+import com.data_management.Patient;
+import com.data_management.PatientRecord;
+
+
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+
+
+public class TriggeredAlertCheckerTest{
+    @Test
+    void testTriggeredAlert(){
+        Patient patient = new Patient(1);
+        List<PatientRecord> records = new ArrayList<>();
+
+        long now = System.currentTimeMillis();
+
+        PatientRecord alertRecord = new PatientRecord(1, 1.0, "Alert", now);
+        records.add(alertRecord);
+
+        TestAlertGenerator generator = new TestAlertGenerator();
+
+        TriggeredAlertChecker checker = new TriggeredAlertChecker();
+        checker.check(patient, records, generator);
+
+        List<Alert> alerts = generator.getAlerts();
+        assertEquals(1, alerts.size());
+        Alert alert = alerts.get(0);
+        assertEquals("Alert, button was triggered.", alert.getCondition());
+        assertEquals("1", alert.getPatientId());
+        assertEquals(now, alert.getTimestamp());
+    }
+    static class TestAlertGenerator extends AlertGenerator {
+        private final List<Alert> alerts = new ArrayList<>();
+        public TestAlertGenerator() {
+            super(new DataStorage());
+        }
+        @Override
+        public void trigger(Alert alert) {
+            alerts.add(alert);
+
+        }
+
+        public List<Alert> getAlerts() {
+            return alerts;
+        }
+    }
+
+}
