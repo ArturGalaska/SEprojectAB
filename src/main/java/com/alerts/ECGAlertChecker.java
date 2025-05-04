e6d31d41deb16f7558ec5202d06cce549ce40f3f
Bartek <b.kaczor@student.maastrichtuniversity.nl>
2025-05-04 19:04:41 +0200

    Added ECGAlertChecker

--- src/main/java/com/alerts/ECGAlertChecker.java
+++ src/main/java/com/alerts/ECGAlertChecker.java
@@ -8,5 +8,28 @@ import java.util.List;
 import java.util.Queue;
 
 public class ECGAlertChecker implements AlertChecker {
-    
+    private static final int WINDOW_SIZE = 5;
+    private static final double THRESHOLD_MULTIPLIER = 2.0;
+
+    @Override
+    public void check(Patient patient, List<PatientRecord> records, AlertGenerator generator){
+        Queue<Double> window = new LinkedList<>();
+
+        for(PatientRecord record : records) {
+            if(!record.getRecordType().equalsIgnoreCase("ECG")) continue;
+
+            double value = record.getMeasurementValue();
+            long time = record.getTimestamp();
+
+            if (window.size() == WINDOW_SIZE) {
+                double average = window.stream().mapToDouble(d -> d).average().orElse(0.0);
+
+                if (value > average * THRESHOLD_MULTIPLIER) {
+                    generator.trigger(new Alert(patient.getPatientId(),"Abnormal ECG Peak", time));
+                }
+                window.poll(); 
+            }
+            window.offer(value);
+        }
+    }
 }
